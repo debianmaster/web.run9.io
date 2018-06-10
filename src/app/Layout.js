ab0b7feab12a620532741c3b919c7245dcde7cec
chakradhar <9chakri@gmail.com>
2018-06-10 17:39:16 +0800

    update

--- src/app/Layout.js
+++ src/app/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Site from './layout/Site'
 import Header from './layout/Header'
 import Content from './layout/Content'
--- src/app/layout/Site.js
+++ src/app/layout/Site.js
@@ -1,5 +1,4 @@
-import styled from 'styled-components'
-
+import styled, { __DO_NOT_USE_OR_YOU_WILL_BE_HAUNTED_BY_SPOOKY_GHOSTS as secret } from 'styled-components'
 const Site = styled.div`
 	display: flex;
 	flex-direction: column;
